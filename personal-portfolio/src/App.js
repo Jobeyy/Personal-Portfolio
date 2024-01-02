9320831842b8ebe4149bd3532cb18ee369e9e56d
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-01-01 23:33:41 -0600

    some css changes and adding fake data to simulate axios call

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,17 +1,37 @@
 import { MainContent } from './components/MainContent';
+import ProjectCard from './components/ProjectCard';
 import { Navbar } from './components/navbar';
 import React from 'react'
 
 function App() {
-
+  const projectCardTest = [
+    {
+      cardImage: "",
+      cardTitle: "Personal Portfolio",
+      cardDescription: "This is testing description",
+      cardLink: "https://github.com/Jobeyy/Personal-Portfolio"
+    },
+    {
+      cardImage: "",
+      cardTitle: "Inventory Management System",
+      cardDescription: "This is a different description for inventory management system",
+      cardLink: "https://github.com/Jobeyy/inventoryManagementSystem"
+    }
+  ]
   
   return (
     <div className='main-container p-0 m-0 flex-column'>
       <div className="row m-0 mb-5">
         <Navbar />
       </div>
-      <div className='row overflow-hidden'>
+      <div className='row overflow-hidden w-100 h-50 mt-5 mb-5'>
         <MainContent />
+      </div>
+      <div className="technologies row w-100 mt-5 mb-5">
+        asdf
+      </div>
+      <div className="row w-100 mt-5 mb-5">
+        <ProjectCard/>
       </div>
     </div>
   );
