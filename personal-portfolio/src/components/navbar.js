4e1e6b178bf59babc2e2c5c69eb7b99b60ab4227
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-01-01 23:34:52 -0600

    Small minor change to button in navbar and adding test image

--- personal-portfolio/src/components/navbar.js
+++ personal-portfolio/src/components/navbar.js
@@ -24,7 +24,7 @@ export function Navbar(){
                 </div>
                     
                 <div className="col-md-4 p-1">
-                  <button className="btn btn-outline-primary" id="button-color">Resume</button>
+                  <button className="btn btn-outline-primary" id="nav-button-color">Resume</button>
                 </div>
               </div>
             </div>
