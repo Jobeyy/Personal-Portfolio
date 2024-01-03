7c3d6d47586cdc76bd0630abd344fee36d6d9503
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-01-03 00:36:26 -0600

    added mapping for projectCardTest

---
+++ personal-portfolio/src/components/Technologies.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function Technologies() {
+    return (
+        <div className='tech'>
+          <ul>
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/color/150/c-plus-plus-logo.png" alt="c-plus-plus-logo"/></a>
+            </li>
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/color/150/python--v1.png" alt="python--v1"/></a>
+            </li>
+            
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/plasticine/150/react.png" alt="react"/></a>
+            </li>
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/ios/150/express-js.png" alt="express-js"/></a>
+            </li>
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/color/150/javascript--v1.png" alt="javascript--v1"/></a>
+            </li>
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/color/150/html-5--v1.png" alt="html-5--v1"/></a>
+            </li>
+            <li>
+              <a href=""><img width="150" height="150" src="https://img.icons8.com/color/150/css3.png" alt="css3"/></a>
+            </li>
+        </ul>
+        </div>
+    )
+}
+
+export default Technologies
