d84a4441e5c688fdc5424350b0586b62899486bf
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-01-03 00:35:05 -0600

    Working on ProjectCard.js and implemented photos per card

--- personal-portfolio/src/components/ProjectCard.js
+++ personal-portfolio/src/components/ProjectCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-function ProjectCard() {
-
+function ProjectCard( { cardImage, cardTitle, cardDescription, cardLink, key } ) {
+  console.log(cardTitle)
 
   return (
     <div className="card project-card m-5">
-      <img className="card-img-top" src="personal-portfolio\static\test1.jpeg" alt="test"/>
+      <img className="card-img-top" src={cardImage} alt="test"/>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-outline-primary " id="project-card-btn">Go somewhere</a>
+        <h5 className="card-title">{cardTitle}</h5>
+        <p className="card-text">{cardDescription}</p>
+        <a href={cardLink} className="btn btn-outline-primary " id="project-card-btn">Go somewhere</a>
       </div>
     </div>
   )
