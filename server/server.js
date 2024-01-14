1fcf1572867a4f2550ada54758fb42e482d73b2e
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-01-13 22:18:23 -0600

    adding required imports for ejs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express')
+const path = require('path');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+const cors = require('cors'); 
+
+const PORT = 8080
+
+let db;
+(async () => {
+	db = await open({
+		filename: 'database.sqlite',
+		driver: sqlite3.Database
+	});
+})();
+
 const app = express()
+app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.json());
+app.use(cors()); 
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.get('/data', async (req, res) => {
+  const data = await db.all(`SELECT * FROM projectCard`)
+  res.json(data)
 })
 
 
 
-app.listen(8080)
+app.listen(PORT)
