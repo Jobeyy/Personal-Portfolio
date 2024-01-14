98b1f41ab12fc38efd94175cee38eafa1b04b990
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-01-13 22:17:35 -0600

    Working on importing pictures

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,33 +1,53 @@
 import { MainContent } from './components/MainContent';
 import ProjectCard from './components/ProjectCard';
 import { Navbar } from './components/navbar';
-import React from 'react'
-import profileImage from './imgs/invManagementSystem.png'
 import Technologies from './components/Technologies';
+import React from 'react'
+import personalPortfolioImage from './imgs/personalPortfolio.png'
+import invManagementImage from './imgs/invManagementSystem.png'
+import chooseAdventureImage from './imgs/chooseAdventureGame.png'
+
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 function App() {
+  
   const projectCardTest = [
     {
-      cardImage: profileImage,
+      cardImage: personalPortfolioImage,
       cardTitle: "Personal Portfolio",
       cardDescription: "Personal Portfolio made using Bootstrap and React to dynamically display previous projects in an engaging way. While providing a seamless navigation experience for visitors to explore my professional journey and projects.",
       cardLink: "https://github.com/Jobeyy/Personal-Portfolio"
     },
     {
-      cardImage: profileImage,
+      cardImage: invManagementImage,
       cardTitle: "Inventory Management System",
       cardDescription: "Inventory Management system created in python used to help business manage their stock levels, and transactions. This system was made to simply the inventory tracking process and ensure that the right amount of stock is in hand.",
       cardLink: "https://github.com/Jobeyy/inventoryManagementSystem"
     },
     {
-      cardImage: profileImage,
+      cardImage: chooseAdventureImage,
       cardTitle: "Choose Your Own Adventure game",
       cardDescription: "Text-based adventure with this C++ console game, where players make pivotal decisions to navigate through a story. Immerse yourself in an interactive narrative, shaping your destiny with each choice in this dynamic and engaging choose-your-own-adventure experience.",
       cardLink: "https://github.com/Jobeyy/inventoryManagementSystem"
     }
   ]
-  
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+          const response = await axios.get('http://localhost:8080/data');
+          setData(response.data);
+      } catch(error) {
+        
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  console.log(data)
   return (
     <div className='main-container p-0 m-0 flex-column main-background'>
       <div className="row" id="nav-bar">
