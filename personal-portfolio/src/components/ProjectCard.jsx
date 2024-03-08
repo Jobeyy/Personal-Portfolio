9d0050e1d773d8e4f2a355697f395ad4067aef1c
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-03-07 22:23:30 -0600

    Changed Text

--- personal-portfolio/src/components/ProjectCard.jsx
+++ personal-portfolio/src/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ function ProjectCard( { cardImage, cardTitle, cardDescription, cardLink, id } ) 
       <div className="card-body">
         <h5 className="card-title">{cardTitle}</h5>
         <p className="card-text">{cardDescription}</p>
-        <a href={cardLink} className="btn btn-outline-primary " id="project-card-btn">Go somewhere</a>
+        <a href={cardLink} className="btn btn-outline-primary " id="project-card-btn">GitHub link</a>
       </div>
     </div>
   )
